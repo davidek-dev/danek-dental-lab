34e208e79f3e559c540952df3fad746e2e6fb667
david.zeplin <zeplindavid@gmail.com>
2023-11-30 12:29:58 +0100

    dq

--- astro.config.js
+++ astro.config.js
@@ -5,8 +5,9 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  publicDir: 'public/',
   outDir: 'dist/',
-  site: 'https://danekdentallab.netlify.app',
+  site: 'https://danekdentallab.netlify.app/',
   integrations: [react(), tailwind()],
   output: "server",
   adapter: netlify({
