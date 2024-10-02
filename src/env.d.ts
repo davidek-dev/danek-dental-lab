59310a90cdcbd5a9b89f53cd3867b0c27dfb17da
david.zeplin <zeplindavid@gmail.com>
2024-10-02 10:05:13 +0200

    idk

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,11 @@
+/// <reference path="../.astro/types.d.ts" />
 interface ImportMetaEnv {
     readonly CONTENTFUL_SPACE_ID: string;
     readonly CONTENTFUL_DELIVERY_TOKEN: string;
     readonly CONTENTFUL_PREVIEW_TOKEN: string;
   }
+
+  // eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="vite/client" />
